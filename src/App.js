117c5c842825985b90af4355dcdd482fdd87056d
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2020-02-21 22:26:24 -0700

    Successful axios request, adding cors anywhere. Starting to set up comic nav buttons

--- src/App.js
+++ src/App.js
@@ -3,9 +3,36 @@ import './App.scss';
 import axios from 'axios';
 
 function App() {
+  const [comic, setComic] = useState(null);
+
+  //IMPORTANT:To avoid being banned or paused in making a network request of a public, put it inside useEffect so the request isn't being made on every render. Or put it in as a callback function that you trigger as well.
+  useEffect(() => {
+    axios.get('https://cors-anywhere.herokuapp.com/http://xkcd.com/info.0.json')
+      .then(res => setComic(res.data))
+      .catch(err => console.log("Oops! An error occurred",err));
+  }, []);
+
+
+//dynamically fetching different comics at will by a specific comic number
+  const fetchComic = (number) => {
+    axios.get(`https://cors-anywhere.herokuapp.com/http://xkcd.com/${number}/info.0.json`)
+      .then(res => setComic(res.data))
+      .catch(err => console.log("Oops! An error occurred",err));
+  };
+
+  console.log(comic)
+  if(!comic) {
+    //loading spinner can go here!
+    return <div>loading...</div>;
+  };
+
   return (
     <div className="app">
-      React XKCD
+      {comic.title}
+      <button onClick={() => fetchComic(comic.num - 1)}>
+        previous
+      </button>
+      <img src={comic.img} alt={comic.alt} />
     </div>
   );
 };
