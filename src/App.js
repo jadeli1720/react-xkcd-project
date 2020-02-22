8bf5ada7aca9d1af47b877391bdec6184913bfaf
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2020-02-21 22:44:26 -0700

    Set up loading state

--- src/App.js
+++ src/App.js
@@ -3,29 +3,45 @@ import './App.scss';
 import axios from 'axios';
 
 function App() {
+  const [loading, setLoading] = useState(true);
   const [comic, setComic] = useState(null);
 
   //IMPORTANT:To avoid being banned or paused in making a network request of a public, put it inside useEffect so the request isn't being made on every render. Or put it in as a callback function that you trigger as well.
   useEffect(() => {
+    setLoading(true)
     axios.get('https://cors-anywhere.herokuapp.com/http://xkcd.com/info.0.json')
-      .then(res => setComic(res.data))
-      .catch(err => console.log("Oops! An error occurred",err));
+      .then(res => {
+        setComic(res.data);
+        setLoading(false);
+      })
+      .catch(err => {
+        console.log("Oops! An error occurred",err);
+        setLoading(false);
+      });
   }, []);
 
 
-//dynamically fetching different comics at will by a specific comic number
+//dynamically fetching different comics at will by a specific comic number  (comic.num)
   const fetchComic = (number) => {
+    setLoading(true)
     axios.get(`https://cors-anywhere.herokuapp.com/http://xkcd.com/${number}/info.0.json`)
       .then(res => setComic(res.data))
-      .catch(err => console.log("Oops! An error occurred",err));
+      .catch(err => console.log("Oops! An error occurred",err))
+      //when either .then() or .catch is done, wrap it up with setLoading to be false
+      .finally(() =>  setLoading(false));
   };
 
   console.log(comic)
-  if(!comic) {
+  if(loading) {
     //loading spinner can go here!
     return <div>loading...</div>;
   };
 
+  if(!comic) {
+    return <div>Oops. I'm sorry but something went wrong!</div>
+  }
+
+  //Remember, for the bottom return statement to run (because a react component is just a function that needs to return JSX), comic cannot be null (it must be have a value). If it is, the component stops at the above conditional and we return loading until a comic becomes !null.
   return (
     <div className="app">
       {comic.title}
