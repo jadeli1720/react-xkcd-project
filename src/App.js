31a59c87580e16730dd18cb7308078216776144d
Jade E Lopez <38263897+jadeli1720@users.noreply.github.com>
2020-02-26 13:56:07 -0700

    Added About and Blog components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
+import Comic from './components/Comic';
+import About from './components/About';
+import Blog from './components/Blog';
 import './App.scss';
 
-import Comic from './components/Comic'
+
 
 function App() {
-  
   return (
     <div className="app">
       <Comic/>
